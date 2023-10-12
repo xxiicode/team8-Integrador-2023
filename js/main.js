9b93f3d76d3db5d5d1e598388ce0448c69870a7c
xxiicode <leandrobilokapic@gmail.com>
2023-10-12 16:49:10 +0200

    primer responsive + nav mobile

---
+++ js/main.js
@@ -0,0 +1,17 @@
+console.log ('Somos Grupo 8');
+
+let ham = document.getElementById("ham");
+let nav = document.getElementById("nav");
+
+ham.addEventListener('click', () => toggle());
+
+function toggle() {
+    if (window.innerWidth <= 430) {
+        let computedStyle = window.getComputedStyle(nav);
+        if (computedStyle.right === "-188px") {
+            nav.style.right = "0px";
+        } else {
+            nav.style.right = "-188px";
+        }
+    }
+}
