ded825847f89048000a3c145413dfbe54aee8bd3
xxiicode <leandrobilokapic@gmail.com>
2023-10-23 17:28:39 +0200

    botones sumar restar, funcionabilidad

---
+++ js/item.js
@@ -0,0 +1,9 @@
+const add = document.querySelector('#item-sumar');
+const subtract = document.querySelector('#item-restar');
+const quantity = document.querySelector('#quantity');
+
+add.addEventListener('click', () => quantity.value = Number(quantity.value) +1);
+subtract.addEventListener('click', () => {
+    if (quantity.value > 0)
+    quantity.value = Number(quantity.value) -1
+});
