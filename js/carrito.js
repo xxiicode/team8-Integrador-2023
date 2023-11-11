8617c6346f83a4029ad53ae3f8841195b0bfbe10
María Agustina Sangiácomo <sangiacomoagus@gmail.com>
2023-11-11 10:03:09 -0300

    cree la rama para hacer mision3 y agregue icono a la pag

--- js/carrito.js
+++ js/carrito.js
@@ -1,9 +1,9 @@
-const add = document.querySelector('#x');
-const subtract = document.querySelector('#x');
-const quantity = document.querySelector('#x');
+// const add = document.querySelector('#');
+// const subtract = document.querySelector('#');
+// const quantity = document.querySelector('#x');
 
-add.addEventListener('click', () => quantity.value = Number(quantity.value) +1);
-subtract.addEventListener('click', () => {
-    if (quantity.value > 0)
-    quantity.value = Number(quantity.value) -1
-});
+// add.addEventListener('click', () => quantity.value = Number(quantity.value) +1);
+// subtract.addEventListener('click', () => {
+//     if (quantity.value > 0)
+//     quantity.value = Number(quantity.value) -1;
+// });
--- js/item.js
+++ js/item.js
@@ -5,5 +5,5 @@ const quantity = document.querySelector('#quantity');
 add.addEventListener('click', () => quantity.value = Number(quantity.value) +1);
 subtract.addEventListener('click', () => {
     if (quantity.value > 0)
-    quantity.value = Number(quantity.value) -1
+    quantity.value = Number(quantity.value) -1;
 });
